fb3582cd2bfb03ce73e7a355e0abf6b8bf79b27d
abdullahshahid137 <abdullahshahid137@gmail.com>
2022-06-24 16:41:41 +0500

    new update after create and update nft

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ import { Connection, clusterApiUrl, Keypair, PublicKey} from "@solana/web3.js";
 
 
 const connection = new Connection(clusterApiUrl("devnet"));
-const wallet = Keypair.generate();
+// const wallet = Keypair.generate();
+
+import wallet_file from './wallet.json' assert {type: "json"};
+const wallet = Keypair.fromSecretKey(Uint8Array.from(wallet_file));
+// console.log('=====> wallet:', wallet)
 
 const metaplex = Metaplex.make(connection)
     .use(keypairIdentity(wallet))
@@ -56,10 +60,9 @@ const getNft_findAllByMintList = async(mintIDs) => {
 
 
 
-const getNft_findAllByOwner = async() => {
+const getNft_findAllByOwner = async(ownerAddress) => {
     // const myNfts = await metaplex.nfts().findAllByOwner(metaplex.identity().publicKey);
-    const myNfts = await metaplex.nfts().findAllByOwner(new PublicKey('6nZRR27JKqK1isskNgtib6n6Und7xuJYPhjWfiSUypEd'));
-    
+    const myNfts = await metaplex.nfts().findAllByOwner(new PublicKey(ownerAddress));
     console.log('=====> getNft_findAllByOwner: ', myNfts.mint, '\n\n');
 
 }
@@ -82,16 +85,12 @@ const getNft_findAllByCandyMachine = async() => {
 }
 
 async function main_get(){
-    await getNft_findByMint("8kzRZtRSGcHDJTfrCKsZfa2neohNGEFYdf7mWvKdx2YB");
+    // await getNft_findByMint("8kzRZtRSGcHDJTfrCKsZfa2neohNGEFYdf7mWvKdx2YB");
     // await getNft_findAllByMintList(["8kzRZtRSGcHDJTfrCKsZfa2neohNGEFYdf7mWvKdx2YB", "8kzRZtRSGcHDJTfrCKsZfa2neohNGEFYdf7mWvKdx2YB"]);
-    // await getNft_findAllByOwner();
+    // await getNft_findAllByOwner('6nZRR27JKqK1isskNgtib6n6Und7xuJYPhjWfiSUypEd');
     // await getNft_findAllByCandyMachine();
 }
 main_get();
-
-
-
-
 
 
 
@@ -116,18 +115,55 @@ const setNft_uploadMetadata = async() => {
 
 const setNft_create = async() => {
     const { nft } = await metaplex.nfts().create({
-        uri: "https://5issneubaq5oi624bgzntcffh5a7dfhjhoeo5y3x2eoorypcqy.arweave.net/6iUmkoEEOuR7XAmy2Yil_P0HxlOk7iO7jd9Ec6OHihk/",
+        uri: "https://ls3cbsrrce2el32w4uwf2kri3c6rbuxi4x2rsl4dzhveomkvo4.arweave.net/XLYgyjERNEXvVuUsXSoo2L0Q0ujl9Rkvg8_nqRzFVd8/",
+        isMutable: true,
     });
     
     console.log('=====> setNft_create: ', nft, '\n\n');
 }
 
 
+async function get_my_nft(){
+    const myNfts = await metaplex.nfts().findAllByOwner(wallet.publicKey);
+    console.log('=====> get my Nft findAllByOwner: ', myNfts.length, '\n\n');
+    const nft = await metaplex.nfts().findByMint(myNfts[myNfts.length-1].mint);
+    console.log('=====> get my Nft findByMint: ', nft, '\n\n********************************************\n\n');
+    
+    return nft;
+}
+
+
+const setNft_update = async(my_nft) => {
+
+    const { nft: updatedNft } = await metaplex.nfts().update(my_nft, {
+        name: "My NFT Name Changed",
+        maxSupply: 10,
+        uri: "https://ls3cbsrrce2el32w4uwf2kri3c6rbuxi4x2rsl4dzhveomkvo4.arweave.net/XLYgyjERNEXvVuUsXSoo2L0Q0ujl9Rkvg8_nqRzFVd8/",
+    });
+
+
+    // const { nft: updatedNft } = await metaplex.nfts().update(nft, {
+    //     uri: newUri,
+    // });
+    
+    // console.log('=====> setNft_update: nft: ', nft, '\n\n');
+    console.log('=====> setNft_update: updatedNft: ', updatedNft, '\n\n');
+}
+
+
+const setNft_printNewEdition = async(my_nft) => {
+
+    const { nft: printedNft } = await metaplex.nfts().printNewEdition(my_nft.mint);
+    console.log('=====> setNft_update: updatedNft: ', printedNft, '\n\n');
+}
+
 async function main_set(){
-    // await setNft_uploadMetadata();
-    await setNft_create();
+    await setNft_uploadMetadata();
+    // await setNft_create();
+    // await get_my_nft();
+    // await setNft_update(await get_my_nft());
+    // await setNft_printNewEdition(await get_my_nft());
 
 }
 main_set();
-
 
